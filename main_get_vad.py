a2f173247bd55e8032ef2c778f923f28650ef74f
Neville Ryant <nryant@gmail.com>
2019-03-14 19:03:33 -0400

    Improve PEP8 compliance.

--- main_get_vad.py
+++ main_get_vad.py
@@ -1,39 +1,65 @@
-# -*- coding:UTF-8 -*-
-from warnings import warn
-import numpy as np
-import webrtcvad
-from librosa.core import resample
-from librosa.util import frame
-from librosa import load
-import utils
+#!/usr/bin/env python
+"""TODO
+
+To change the aggressiveness mode, how aggressive WebRTC is about filtering out
+non-speech, use the ``--mode`` parameter with an integer in the range [0, 3]:
+
+    python main_get_vad.py --mode 0 --wav_dir some_dir
+
+Higher values for ``--mode`` translate into more aggressive filtering.
+
+"""
+from __future__ import print_function
+from __future__ import unicode_literals
 import argparse
 import os
+import sys
 
-#main_vad(wav_dir = args.wav_dir, mode = args.mode, hop= args.hoplength)
-def main_vad(wav_dir ,mode, hop):
+from librosa import load
+
+import utils
+
+
+def main_vad(wav_dir, mode, hoplength):
+    """TODO"""
     if not os.path.exists(wav_dir):
-        raise RuntimeError("cannot locate the original dictionary !")
+        raise RuntimeError('cannot locate the original dictionary !')
 
-    wav_files = [os.path.join(wav_dir,line)  for line in os.listdir(wav_dir) ]
-
+    wav_files = [os.path.join(wav_dir, line)  for line in os.listdir(wav_dir)]
+    wav_files = sorted(wav_files)
     for wav in wav_files:
         if wav.endswith('.wav'):
             data, fs = load(wav, sr=16000)
-            vad_info = utils.vad(data, fs, fs_vad = 16000, hoplength = hop, vad_mode=mode)
+            vad_info = utils.vad(
+                data, fs, fs_vad=16000, hoplength=hoplength, vad_mode=mode)
+            segments = utils.get_segments(vad_info, fs)
+            with open(wav.replace('.wav', '.sad'), 'w') as f:
+                for i in range(segments.shape[0]):
+                    start_time = segments[i][0]
+                    end_time = segments[i][1]
+                    f.write('%.3f  %.3f \n' % (start_time, end_time))
 
-            segments = utils.get_segments(vad_info,fs)
-            output_file = open(wav.replace('.wav','.sad' ),'w')
-            for i in range(segments.shape[0]):
-                start_time = segments[i][0]
-                end_time =  segments[i][1]
-                output_file.write( "%.3f  %.3f \n" %(start_time, end_time) )
+
+def main():
+    """Main."""
+    parser = argparse.ArgumentParser(
+        description='Perform VAD using webrtcvad.', add_help=True)
+    parser.add_argument(
+        '--wav_dir', nargs=None, default=None, type=str, metavar='STR',
+        help='directory containing WAV files to perform VAD for '
+             '(default: %(default)s)')
+    parser.add_argument(
+        '--mode', nargs=None, default=3, type=int, metavar='INT',
+        help='WebRTC VAD aggressiveness (default: %(default)s)')
+    parser.add_argument(
+        '--hoplength', nargs=None, default=30, type=int, metavar='INT',
+        help='duration between frames in ms (default: %(default)s)')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    args = parser.parse_args()
+    main_vad(wav_dir=args.wav_dir, mode=args.mode, hoplength=args.hoplength)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Getting VAD information based on webrtcvad module.')
-    parser.add_argument('--wav_dir',type=str, default=None)
-    parser.add_argument('--mode', type=int, default=3)
-    parser.add_argument('--hoplength',type=int,default=30)
-    args = parser.parse_args()
-
-    main_vad(wav_dir = args.wav_dir, mode = args.mode, hop= args.hoplength)
+    main()
--- utils.py
+++ utils.py
@@ -1,147 +1,173 @@
+"""Various utility functions."""
+from __future__ import print_function
+from __future__ import unicode_literals
 import struct
-import scipy.io.wavfile
-import os
-import numpy as np
-from scipy import signal
-import webrtcvad
+import sys
+
 from librosa.core import resample
 from librosa.util import frame
-from librosa import load
-import pdb  
+import numpy as np
+import webrtcvad
+
+EPS = 1e-8
 
 
-def stft(x, window, nperseg=512, noverlap=256):
-    if len(window) != nperseg:
-        raise ValueError('window length must equal nperseg')
+def warn(msg):
+    """Print warning message to STERR."""
+    msg = 'WARN: %s' % msg
+    print(msg, file=sys.stderr)
+
+
+# TODO: See if there is any reason for not just calling librosa.core.stft.
+# n_per_seg = nfft
+# should default to window.size
+def stft(x, window, n_per_seg=512, noverlap=256):
+    """Return short-time Fourier transform (STFT) for signal.
+
+    Parameters
+    ----------
+    x : ndarray, (n_samps,)
+        Input signal.
+
+    window : ndarray, (wl,)
+        Array of weights to use when windowing the signal.
+
+    n_per_seg : int, optional
+
+    """
+    if len(window) != n_per_seg:
+        raise ValueError('window length must equal n_per_seg')
     x = np.array(x)
-    nadd = noverlap - (len(x) - nperseg) % noverlap
+    nadd = noverlap - (len(x) - n_per_seg) % noverlap
     x = np.concatenate((x, np.zeros(nadd)))
-    step = nperseg - noverlap
-    shape = x.shape[:-1] + ((x.shape[-1] - noverlap) // step, nperseg)
+    step = n_per_seg - noverlap
+    shape = x.shape[:-1] + ((x.shape[-1] - noverlap) // step, n_per_seg)
     strides = x.strides[:-1] + (step * x.strides[-1], x.strides[-1])
     x = np.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
     x = x * window
-    result = np.fft.rfft(x, n=nperseg)
+    result = np.fft.rfft(x, n=n_per_seg)
     return result
 
 
-def istft(x, window, nperseg=512, noverlap=256):
+def istft(x, window, n_per_seg=512, noverlap=256):
+    """TODO"""
     x = np.fft.irfft(x)
-    y = np.zeros((len(x) - 1) * noverlap + nperseg)
+    y = np.zeros((len(x) - 1) * noverlap + n_per_seg)
     C1 = window[0:256]
     C2 = window[0:256] + window[256:512]
     C3 = window[256:512]
-    y[0:noverlap] = x[0][0:noverlap] / C1   
+    y[0:noverlap] = x[0][0:noverlap] / C1
     for i in range(1, len(x)):
-        y[i * noverlap:(i + 1) * noverlap] = (x[i - 1][noverlap:nperseg] + x[i][0:noverlap]) / C2
+        y[i * noverlap:(i + 1) * noverlap] = (x[i - 1][noverlap:n_per_seg] + x[i][0:noverlap]) / C2
     y[-noverlap:] = x[len(x) - 1][noverlap:] / C3
     # return y[0:len(wave)]
     return y
 
 
-def wav2logspec(x, window, nperseg=512, noverlap=256):
-    y = stft(x, window, nperseg=nperseg, noverlap=noverlap)
-    return np.log(np.square(abs(y))+1e-8)
+def wav2logspec(x, window, n_per_seg=512, noverlap=256):
+    """TODO"""
+    y = stft(x, window, n_per_seg=n_per_seg, noverlap=noverlap)
+    return np.log(np.square(abs(y)) + EPS)
 
 
-def logspec2wav(lps, wave, window, nperseg=512, noverlap=256):
+def logspec2wav(lps, wave, window, n_per_seg=512, noverlap=256):
+    "Convert log-power spectrum back to time domain."""
     z = stft(wave, window)
-    angle = z / (np.abs(z) + 1e-8)
+    angle = z / (np.abs(z) + EPS) # Recover phase information
     x = np.sqrt(np.exp(lps)) * angle
     x = np.fft.irfft(x)
-    y = np.zeros((len(x)-1) * noverlap + nperseg)
+    y = np.zeros((len(x) - 1) * noverlap + n_per_seg)
     C1 = window[0:256]
     C2 = window[0:256] + window[256:512]
     C3 = window[256:512]
-    y[0:noverlap] = x[0][0:noverlap] / C1  #
+    y[0:noverlap] = x[0][0:noverlap] / C1
     for i in range(1, len(x)):
-        y[i*noverlap:(i+1)*noverlap] = (x[i-1][noverlap:nperseg] + x[i][0:noverlap]) / C2
+        y[i*noverlap:(i + 1)*noverlap] = (x[i-1][noverlap:n_per_seg] + x[i][0:noverlap]) / C2
     y[-noverlap:] = x[len(x)-1][noverlap:] / C3
-    # return y[0:len(wave)]
     return np.int16(y[0:len(wave)])
 
-    
-    
+
+MAX_PCM_VAL = 32767
 def peak_normalization(wave):
+    """Perform peak normalization."""
     norm = wave.astype(float)
-    norm = norm / ( max (abs(norm))) * (np.exp2(15)-1)
+    norm = norm / max(abs(norm)) * MAX_PCM_VAL
     return norm.astype(int)
 
-def readHtk(filename):
-    """
-    Reads the features in a HTK file, and returns them in a 2-D numpy array.
-    """
-    with open(filename, "rb") as f:
+
+def read_htk(filename):
+    """Return features from HTK file a 2-D numpy array."""
+    with open(filename, 'rb') as f:
         # Read header
-        nSamples, sampPeriod, sampSize, parmKind = struct.unpack(">iihh", f.read(12))
+        n_samples, samp_period, samp_size, parm_kind = struct.unpack(
+            '>iihh', f.read(12))
+
         # Read data
-        data = struct.unpack(">%df" % (nSamples * sampSize / 4), f.read(nSamples * sampSize))
-      #  return numpy.array(data).reshape(nSamples, int(sampSize / 4))
-        return nSamples, sampPeriod, sampSize, parmKind, data
+        data = struct.unpack(
+            '>%df' % (n_samples * samp_size / 4), f.read(n_samples * samp_size))
 
-def writeHtk(filename, feature, sampPeriod, parmKind):
-    """
-    Writes the features in a 2-D numpy array into a HTK file.
-    """
-    with open(filename, "wb") as f:
+        return n_samples, samp_period, samp_size, parm_kind, data
+
+
+def write_htk(filename, feature, samp_period, parm_kind):
+    """Write array of frame-level features to HTK binary file."""
+    with open(filename, 'wb') as f:
         # Write header
-        nSamples = feature.shape[0]
-        sampSize = feature.shape[1] * 4
-        f.write(struct.pack(">iihh", nSamples, sampPeriod, sampSize, parmKind))
-        f.write(struct.pack(">%df" % (nSamples * sampSize / 4), *feature.ravel()))
-       
-        
+        n_samples = feature.shape[0]
+        samp_size = feature.shape[1] * 4
+        f.write(struct.pack('>iihh', n_samples, samp_period, samp_size, parm_kind))
+        f.write(struct.pack('>%df' % (n_samples * samp_size / 4), *feature.ravel()))
 
+
+VALID_VAD_SRS = {8000, 16000, 32000, 48000}
+VALID_VAD_HOPLENGTHS = {10, 20, 30}
+VALID_VAD_MODES = {0, 1, 2, 3}
 def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
     """ Voice activity detection.
+
     This was implementioned for easier use of py-webrtcvad.
+
     Thanks to: https://github.com/wiseman/py-webrtcvad.git
+
     Parameters
     ----------
-    data : ndarray
-        numpy array of mono (1 ch) speech data.
-        1-d or 2-d, if 2-d, shape must be (1, time_length) or (time_length, 1).
-        if data type is int, -32768 < data < 32767.
-        if data type is float, -1 < data < 1.
+    data : ndarray, (n_samps,)
+        Input signal.
+
     fs : int
-        Sampling frequency of data.
+        Sample rate in Hz of ``data``.
+
     fs_vad : int, optional
-        Sampling frequency for webrtcvad.
-        fs_vad must be 8000, 16000, 32000 or 48000.
-        Default is 16000.
+        Sample rate in Hz for webrtcvad.
+        (Default: 16000)
+
     hoplength : int, optional
-        Step size[milli second].
-        hoplength must be 10, 20, or 30.
-        Default is 0.1.
+        Step size in milliseconds.
+        (Default: 30)
+
     vad_mode : int, optional
-        set vad aggressiveness.
-        As vad_mode increases, it becomes more aggressive.
-        vad_mode must be 0, 1, 2 or 3.
-        Default is 0.
+        Set vad aggressiveness. As ``vad_mode`` increases, it becomes more
+        aggressive.
+        (Default: 0)
+
     Returns
     -------
     vact : ndarray
-        voice activity. time length of vact is same as input data.
-        If 0, it is unvoiced, 1 is voiced.
+        voice activity. time length of vact is same as input data. If 0, it is
+        unvoiced, 1 is voiced.
     """
-
-    # check argument
-    if fs_vad not in [8000, 16000, 32000, 48000]:
-        raise ValueError('fs_vad must be 8000, 16000, 32000 or 48000.')
-
-    if hoplength not in [10, 20, 30]:
-        raise ValueError('hoplength must be 10, 20, or 30.')
-
-    if vad_mode not in [0, 1, 2, 3]:
-        raise ValueError('vad_mode must be 0, 1, 2 or 3.')
-
-    # check data
+    # Check arguments.
+    if fs_vad not in VALID_VAD_SRS:
+        raise ValueError('fs_vad must be one of %s' % VALID_VAD_SRS)
+    if hoplength not in VALID_VAD_HOPLENGTHS:
+        raise ValueError('hoplength must be one of %s' % VALID_VAD_FRAME_LENGTHS)
+    if vad_mode not in VALID_VAD_MODES:
+        raise ValueError('vad_mode must be one of %s' % VALID_VAD_MODES)
     if data.dtype.kind == 'i':
         if data.max() > 2**15 - 1 or data.min() < -2**15:
             raise ValueError(
-                'when data type is int, data must be -32768 < data < 32767.')
+                'when data type is int, data must be in range [-32768, 32767].')
         data = data.astype('f')
-
     elif data.dtype.kind == 'f':
         if np.abs(data).max() >= 1:
             data = data / np.abs(data).max() * 0.9
@@ -149,74 +175,52 @@ def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
         data = (data * 2**15).astype('f')
     else:
         raise ValueError('data dtype must be int or float.')
-
     data = data.squeeze()
     if not data.ndim == 1:
         raise ValueError('data must be mono (1 ch).')
 
-    # resampling
+    # Resample.
     if fs != fs_vad:
         resampled = resample(data, fs, fs_vad)
     else:
         resampled = data
-
     resampled = resampled.astype('int16')
-
-    hop = fs_vad * hoplength // 1000
+ 
+    # Perform VAD using resampled data.
+    hop = fs_vad * hoplength // 1000   
     framelen = resampled.size // hop + 1
     padlen = framelen * hop - resampled.size
-    paded = np.lib.pad(resampled, (0, padlen), 'constant', constant_values=0)
-    framed = frame(paded, frame_length=hop, hop_length=hop).T
-    
-    
+    padded = np.lib.pad(resampled, (0, padlen), 'constant', constant_values=0)
+    framed = frame(padded, frame_length=hop, hop_length=hop).T
     vad = webrtcvad.Vad()
     vad.set_mode(vad_mode)
     valist = [vad.is_speech(tmp.tobytes(), fs_vad) for tmp in framed]
-
     hop_origin = fs * hoplength // 1000
     va_framed = np.zeros([len(valist), hop_origin])
     va_framed[valist] = 1
 
     return va_framed.reshape(-1)[:data.size]
-    
-    
-    
-    
 
-def get_segments(vad_info,fs):
-    vad_index = np.where( vad_info==1.0) # find the speech index   
-    vad_diff = np.diff( vad_index)
-            
+
+def get_segments(vad_info, fs):
+    """Convert array of VAD labels into segmentation."""
+    vad_index = np.where(vad_info == 1.0) # Find the speech index.
+    vad_diff = np.diff(vad_index)
+
     vad_temp = np.zeros_like(vad_diff)
-    vad_temp[ np.where(vad_diff==1) ]  = 1       
-    vad_temp =  np.column_stack( (np.array([0]), vad_temp, np.array([0]) ))
+    vad_temp[np.where(vad_diff == 1)] = 1
+    vad_temp = np.column_stack((np.array([0]), vad_temp, np.array([0])))
     final_index = np.diff(vad_temp)
-            
-    starts = np.where( final_index == 1)
-    ends = np.where( final_index == -1)
-      
-    sad_info = np.column_stack( (starts[1], ends[1]) ) 
+
+    starts = np.where(final_index == 1)
+    ends = np.where(final_index == -1)
+
+    sad_info = np.column_stack([starts[1], ends[1]])
     vad_index = vad_index[0]
-    
-    segments = np.zeros_like(sad_info,dtype=np.float)
+
+    segments = np.zeros_like(sad_info, dtype=np.float)
     for i in range(sad_info.shape[0]):
-        segments[i][0] = float( vad_index [ sad_info[i][0] ] ) / fs
-        segments[i][1] =  float( vad_index[ sad_info[i][1] ]  +1 ) / fs
- 
-    return  segments  # present in seconds
+        segments[i][0] = float(vad_index[sad_info[i][0]]) / fs
+        segments[i][1] = float(vad_index[sad_info[i][1]] + 1) / fs
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return segments  # Present in seconds.
